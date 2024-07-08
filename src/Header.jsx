48c69b049427427d3017b6ee9d033d977d245ead
Caio-Milhomem <caiomilhomem@outlook.com.br>
2024-07-08 17:26:08 -0300

    Melhoras na NavBar

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,14 +2,14 @@
 function Header() {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top py-0 border border-secondary border-2">
-            <div className="container my-1">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top py-0 border border-secondary border-2 rounded-5 w-75 mx-auto">
+            <div className="container justify-content-center  my-1">
                 <a className="navbar-brand fw-bolder" style={{ letterSpacing: "0.02em" }} href="#">Caio Milhomem</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler border-2 border-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse fw-bolder" id="navbarResponsive">
-                    <ul className="navbar-nav ms-auto">
+                <div className="collapse navbar-collapse fw-bolder flex-grow-0 ms-auto" id="navbarResponsive">
+                    <ul className="navbar-nav text-center">
                         <li className="nav-item"><a className="nav-link active" href="#home">Home</a></li>
                         <li className="nav-item"><a className="nav-link" href="#sobre">Sobre mim</a></li>
                         <li className="nav-item"><a className="nav-link" href="#tecnologias">Tecnologias</a></li>
