129872a0f0bbe22e7abe41cb1f27fe121caca452
Caio-Milhomem <caiomilhomem@outlook.com.br>
2024-07-09 16:17:02 -0300

    Melhoras na seção tecnologias

--- src/Tecnologias.jsx
+++ src/Tecnologias.jsx
@@ -2,48 +2,81 @@
 function Tecnologias() {
 
     return (
-        <section className="tecnologias vh-100 pt-3" id="tecnologias">
+        <section className="tecnologias min-vh-100 pt-3" id="tecnologias">
             <h2 className="text-white text-center mt-5 mb-3 fw-bolder" style={{ textShadow: "2px 2px black" }}>Tecnologias</h2>
             <div className="container  text-white d-flex justify-content-center">
                 <div className="container d-flex flex-column justify-content-center">
-                    <div className="row">
-                        <div className="col text-center">
-                            <img className="img-fluid" src="/img/bootstrapLogo.png" alt="bootStrap5Logo" />
-                            <p>Bootstrap</p>
+                    <div className="row  row-cols-2 row-cols-md-4  mb-2">
+                        <div className="col text-center ">
+                            <div className="card tec-card">
+                                <div className="card-body p-0">
+                                    <img className="img-fluid" style={{ maxHeight: "10rem" }} src="/img/bootstrapLogo.png" alt="bootStrap5Logo" />
+                                    <h3 className="card-title text-white" style={{ textShadow: "2px 2px rgb(66, 66, 66)" }}>Bootstrap</h3>
+                                </div>
+                            </div>
                         </div>
                         <div className="col text-center">
-                            <img className="img-fluid" src="/img/htmlLogo.png" alt="html5Logo" />
-                            <p>HTML</p>
+                            <div className="card tec-card">
+                                <div className="card-body p-0">
+                                    <img className="img-fluid" style={{ maxHeight: "10rem" }} src="/img/htmlLogo.png" alt="html5Logo" />
+                                    <h3 className="card-title text-white" style={{ textShadow: "2px 2px rgb(66, 66, 66)" }}>HTML</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col text-center ">
+                            <div className="card tec-card">
+                                <div className="card-body p-0">
+                                    <img className="img-fluid" style={{ maxHeight: "10rem" }} src="/img/javascriptLogo.png" alt="javascriptLogo" />
+                                    <h3 className="card-title text-white" style={{ textShadow: "2px 2px rgb(66, 66, 66)" }}>JavaScript</h3>
+                                </div>
+                            </div>
                         </div>
                         <div className="col text-center">
-                            <img className="img-fluid" src="/img/javascriptLogo.png" alt="javascriptLogo" />
-                            <p>JavaScript</p>
-                        </div>
-                        <div className="col text-center">
-                            <img className="img-fluid" src="/img/cssLogo.png" alt="cssLogo" />
-                            <p>CSS</p>
+                            <div className="card tec-card">
+                                <div className="card-body p-0">
+                                    <img className="img-fluid" style={{ maxHeight: "10rem" }} src="/img/cssLogo.png" alt="cssLogo" />
+                                    <h3 className="card-title text-white" style={{ textShadow: "2px 2px rgb(66, 66, 66)" }}>CSS</h3>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="row">
+                    <div className="row row-cols-2 row-cols-md-4 pb-3">
                         <div className="col text-center">
-                            <img className="img-fluid" src="/img/c++Logo.png" alt="c++Logo" />
-                            <p>C++</p>
+                            <div className="card tec-card">
+                                <div className="card-body p-0">
+                                    <img className="img-fluid" style={{ maxHeight: "10rem" }} src="/img/c++Logo.png" alt="c++Logo" />
+                                    <h3 className="card-title text-white" style={{ textShadow: "2px 2px rgb(66, 66, 66)" }}>C++</h3>
+                                </div>
+                            </div>
                         </div>
                         <div className="col text-center">
-                            <img className="img-fluid" src="/img/gitLogo.png" alt="gitLogo" />
-                            <p>Git</p>
+                            <div className="card tec-card">
+                                <div className="card-body p-0">
+                                    <img className="img-fluid" style={{ maxHeight: "10rem" }} src="/img/gitLogo.png" alt="gitLogo" />
+                                    <h3 className="card-title text-white" style={{ textShadow: "2px 2px rgb(66, 66, 66)" }}>Git</h3>
+                                </div>
+                            </div>
                         </div>
                         <div className="col text-center">
-                            <img className="img-fluid" src="/img/javaLogo.png" alt="javaLogo" />
-                            <p>Java</p>
+                            <div className="card tec-card">
+                                <div className="card-body p-0">
+                                    <img className="img-fluid" style={{ maxHeight: "10rem" }} src="/img/javaLogo.png" alt="javaLogo" />
+                                    <h3 className="card-title text-white" style={{ textShadow: "2px 2px rgb(66, 66, 66)" }}>Java</h3>
+                                </div>
+                            </div>
                         </div>
                         <div className="col text-center">
-                            <img className="img-fluid" src="/img/postgresqlLogo.png" alt="postgresqlLogo" />
-                            <p>PostgreSQL</p>
+                            <div className="card tec-card">
+                                <div className="card-body p-0">
+                                    <img className="img-fluid" style={{ maxHeight: "10rem" }} src="/img/postgresqlLogo.png" alt="postgresqlLogo" />
+                                    <h3 className="card-title text-white" style={{ textShadow: "2px 2px rgb(66, 66, 66)" }}>PostgreSQL</h3>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </section>
     );
 }
